44b8541c2357984cdab5621affa449d15d0604d7
Tmcity <2891051445@qq.com>
2022-01-22 22:53:44 +0800

    v0.1.3更新
    ·为方块添加了物理效果
    ·调整了碰撞检测的方法
    ·添加了方块与小球碰撞后消失的功能

---
+++ Assets/脚本/碰撞检测.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 碰撞检测 : MonoBehaviour
+{
+
+    public GameObject DestroyObj;
+    //public GameObject TriggerObj;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider other){    //让碰撞发生时
+        Destroy(DestroyObj.gameObject);     //销毁“DestoyObj”
+    }
+}
--- Assets/脚本/移动控制.cs
+++ Assets/脚本/移动控制.cs
@@ -1,12 +1,10 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour
+public class 移动控制 : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    public GameObject sphere;
     void Start()
     {
         
@@ -16,7 +14,6 @@ public class move : MonoBehaviour
     void Update()
     {
         Move();
-        PenZhuang();
     }
     public void Move(){
         if(Input.GetKeyDown(KeyCode.W)&&transform.position.z!=1){//检测W键按下，并同时z不等于1
@@ -30,11 +27,6 @@ public class move : MonoBehaviour
         }
         if(Input.GetKeyDown(KeyCode.D)&&transform.position.x!=1){//检测D键按下，并同时x不等于1
             transform.Translate(Vector3.right);//按下D键时，控制对象向右
-        }
-    }
-    public void PenZhuang(){
-        if(transform.position==sphere.transform.position){//检测当对象的位置等于sphere对象的位置时
-            print("Game Over");//输出“Game Over”
         }
     }
 }
